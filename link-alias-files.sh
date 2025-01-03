29cb412024d021c2bf550288b5a622ecc226067b
Chinh Tran <ctran6@gmail.com>
2025-01-03 13:27:51 -0500

    removed unnecessary comments

--- link-alias-files.sh
+++ link-alias-files.sh
@@ -6,8 +6,5 @@ mv ~/.bash_aliases ~/.bash_aliases.bk
 # Link files in home directory to files in this folder
 ln -s ~/.git/sync-bash-alias/.bashrc ~/.bashrc
 ln -s ~/.git/sync-bash-alias/.bash_aliases ~/.bash_aliases
-<<<<<<< HEAD
-=======
 
->>>>>>> a3e86e448ba51d714b7a0f979b7cb8a8de504352
 source ~/.bashrc
